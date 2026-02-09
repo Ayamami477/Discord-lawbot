770f8b6ee4d6a537e98d68354dff5a34087e3819
MatchaLatte <86759216+Ayamami477@users.noreply.github.com>
2026-02-09 12:39:26 +0000

    基礎2

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import os
+import discord
 import requests
 from typing import Any
+
+intent = discord.Intents.all()
+client = discord.Client(intents=intent)
 
 APPLICATION_ID = os.getenv("APPLICATION_ID")
 BOT_TOKEN = os.getenv("DISCORD_TOKEN")
 PUBLIC_KEY = os.getenv("DISCORD_PUBLIC_KEY")
 BASE_URL = "https://laws.e-gov.go.jp/api/2"
-
 
 response = requests.get(f"{BASE_URL}/laws")
 
@@ -36,11 +39,32 @@ def get_law_data(law_id:str, law_name:str) -> dict[str, str]:
     }
     
 for law in laws_list:
-    law_name = law.get("current_Revision", {}).get("law_title")
+    law_name = law.get("current_revision", {}).get("law_title")
     law_id = law.get("law_info",{}).get("law_id")
     if law_name in LAW_MASTER:
         law_dict[law_name] = get_law_data(law_id, law_name)
     print(f"Available laws: {list(law_dict.keys())}")   
 
 
-
+@client.event
+async def on_ready():
+   print("Ready!")
+  
+@client.event
+async def on_message(message: discord.Message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith("!ping"):
+        await message.channel.send("Pong!")
+    
+    if message.content.startswith("!law"):
+        response_message = "Available laws:\n"
+        for law_name in law_dict.keys():
+            response_message += f"- {law_name}\n"
+        await message.channel.send(response_message)
+ 
+if BOT_TOKEN is  None:
+    raise ValueError("Error: DISCORD_TOKEN environment variable is not set.")       
+        
+client.run(BOT_TOKEN)
